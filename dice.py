24874117ef7b44c6fbed890fe7ab5bc2203e4344
Jackson W. Martin <jackm2580@gmail.com>
2015-06-15 15:46:26 -0400

    save

--- dice.py
+++ dice.py
@@ -3,16 +3,13 @@ import random as r
 def roll(howmany=1,sides=6):
     total=0
     for count in range(howmany):
-        total += rolldie(sides)#works off of rolldie funct.     
+        total += r.randint(1,sides)     
     return total
-
-def rolldie(sides=6):#Makes sides defaultly set to 6
-    return r.randint(1,sides)
 
 def parse_roll(text):
     """Parses traditional dice notation (ex: 3d6)"""
     (howmany, sides) = parse(text)
-    return roll(howmany, sides)
+    return roll(howmany,sides)
 
 def parse(text):
     """Parses traditional dice notation (ex: 3d6)"""
@@ -21,29 +18,29 @@ def parse(text):
 
 if __name__=='__main__':
     import colors as c
-    print(parse_roll('2d10'))
-    exit()
     def testrolldie(sides):
+        print(c.red+'roll(1,{}):'.format(sides)+c.reset)
         for count in range(20):
-            foo=rolldie(6)
-            print(c.red + str(foo), end=' ')
-
-        print(c.clear)
-
-        testrolldie(6)
-        testrolldie(20)
-        testrolldie(100)
-
-        print(c.yellow +'' +c.reset)
-        for count in range(20):
-            foo=roll()
-            print(c.random_color()+str(foo)+c.reset,end=' ')
+            foo=roll(1,sides)
+            print(c.red + str(foo) + c.reset,end=' ')
         print()
         print()
+    print(c.clear)
 
-        for text in ['2d6', '4d6', '1d20','2d100',]:
-            print("parse({})".format(text))
-            print(parse(text))
-            print('parse_roll({})'.format(text))
-            print(parse_roll(text))
+    testrolldie(6)
+    testrolldie(20)
+    testrolldie(100)
+
+    print(c.yellow +'roll():' +c.reset)
+    for count in range(20):
+        foo=roll()
+        print(c.green+str(foo)+c.reset,end=' ')
+    print()
+    print()
+
+    for text in ['2d6', '4d6', '1d20','2d100',]:
+        print("parse({})".format(text))
+        print(parse(text))
+        print('parse_roll({})'.format(text))
+        print(parse_roll(text))
 
