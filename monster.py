a8e300eb7e6cd2a4e48bee513f445270bdc49778
Jackson W. Martin <jackm2580@gmail.com>
2015-07-20 16:31:17 -0400

    save

--- monster.py
+++ monster.py
@@ -1,19 +1,32 @@
 from dice import roll
 from interfaces import HasStats
-
-monsters=[
-'Zombie'
-'Ninja'
-]
+import random
 
 class Zombie(HasStats):
+    title="Zombie"
     def __init__(self):
         self.set_stats()
         self.strength += 10
-
+    def action_attack():
+        pass
 
 class Ninja(HasStats):
+    title="Ninja"
     def __init__(self):
         self.set_stats()
         self.dexterity += 10
+monsters=[
+    Ninja,
+    Zombie
+]
 
+def pick_random():
+    Monster=random.choice(monsters)
+    return Monster()
+if __name__ == "__main__":
+    zomzom=Zombie()
+    print(zomzom.title)
+    zomzom.show_stats()
+    
+    randmon=pick_random()
+    
