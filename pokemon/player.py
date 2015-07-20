b11ba7e64a8df2f8a74bf7b908ae7f63d09879c2
Jackson W. Martin <jackm2580@gmail.com>
2015-07-20 17:15:22 -0400

    save

--- pokemon/player.py
+++ pokemon/player.py
@@ -1,16 +1,21 @@
 from dice import roll
 from interfaces import HasStats
+from utils.py import ask
 import colors as c
+
 class Player(HasStats):
     def __init__(self):
         self.set_stats()
+    def pick_move(self):
+        move=ask("What move do you want to make?")
 
 
 if __name__=='__main__':
-    p1=Player()
+    player=Player()
     print(c.clear)
-    p1.save()
-    p1.load()
-    p1.show_stats()
+    player.save()
+    #player.load()
+    player.show_stats()
+    player.pick_move()
 
 
